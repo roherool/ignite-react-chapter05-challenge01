e9218eab3acd732d64dd806c20767e50feccf2dd
Roberto Oliveira <roberto@streamline.com.br>
2021-10-11 16:32:59 -0300

    Updating the challenge repository

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,38 +1,121 @@
+import { memo, useCallback, useEffect, useState } from "react";
+import { Grid, GridCellRenderer } from "react-virtualized";
+
+import { Header } from "./Header";
 import { MovieCard } from "./MovieCard";
+import { api } from "../services/api";
+
+import "../styles/content.scss";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
 
 interface ContentProps {
-  selectedGenre: {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
+  selectedGenreId: number;
+}
+
+export function ContentComponent({ selectedGenreId }: ContentProps) {
+  const [movies, setMovies] = useState<Array<MovieProps[]>>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        generateData(response.data);
+      });
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  const generateData = useCallback((movies) => {
+    const rows: Array<MovieProps[]> = [];
+    var moviesIndex = 0;
+
+    for (let i = 0; i < Math.ceil(movies.length / 3); i++) {
+      rows[i] = [];
+
+      for (let j = 0; j < 3; j++) {
+        if (movies[moviesIndex] !== undefined) {
+          rows[i].push(movies[moviesIndex]);
+          moviesIndex++;
+        }
+      }
+    }
+    return setMovies(rows);
+  }, []);
+
+  const cellRenderer: GridCellRenderer = ({
+    columnIndex,
+    key,
+    rowIndex,
+    style,
+  }) => {
+    if (movies[rowIndex][columnIndex] !== undefined) {
+      return (
+        <div
+          key={key}
+          style={{
+            ...style,
+            display: "flex",
+            alignItems: "start",
+            justifyContent: "start",
+          }}
+        >
+          <MovieCard
+            key={movies[rowIndex][columnIndex]?.imdbID}
+            title={movies[rowIndex][columnIndex]?.Title}
+            poster={movies[rowIndex][columnIndex]?.Poster}
+            runtime={movies[rowIndex][columnIndex]?.Runtime}
+            rating={movies[rowIndex][columnIndex]?.Ratings[0].Value}
+          />
+        </div>
+      );
+    }
   };
 
-  movies: Array<{
-    imdbID: string;
-    Title: string;
-    Poster: string;
-    Ratings: Array<{
-      Source: string;
-      Value: string;
-    }>;
-    Runtime: string;
-  }>;
-}
-
-export function Content({ selectedGenre, movies }: ContentProps) {
   return (
     <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
-
+      <Header selectedGenre={selectedGenre} />
       <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
+        <Grid
+          cellRenderer={cellRenderer}
+          columnCount={3}
+          rowCount={movies.length}
+          height={770}
+          width={840}
+          rowHeight={420}
+          columnWidth={303}
+          style={{
+            overflowX: "hidden",
+            overflowY: "hidden",
+          }}
+        />
       </main>
     </div>
-  )
+  );
 }
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.selectedGenreId, nextProps.selectedGenreId);
+});
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,36 +1,68 @@
+import { useEffect, useState, useCallback, memo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
+
+import { api } from "../services/api";
+
 import { Button } from "./Button";
 
-interface SideBarProps {
-  genres: Array<{
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-  }>;
-  selectedGenreId: number;
-  buttonClickCallback: (args: any) => void;
+import "../styles/sidebar.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
 }
 
-export function SideBar({
-  genres,
+interface SideBarProps {
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+}
+
+function SideBarComponent({
   selectedGenreId,
-  buttonClickCallback
+  setSelectedGenreId,
 }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id);
+  }, []);
+
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div className="buttons-container" key={String(key)} style={style}>
+        <Button
+          title={genres[index].title}
+          iconName={genres[index].name}
+          onClick={() => handleClickButton(genres[index].id)}
+          selected={selectedGenreId === genres[index].id}
+        />
+      </div>
+    );
+  };
+
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
-
-      <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
-      </div>
-
+      <span>
+        Watch<p>Me</p>
+      </span>
+      <List
+        height={470}
+        rowHeight={78}
+        width={318}
+        rowCount={genres.length}
+        rowRenderer={rowRenderer}
+      />
     </nav>
-  )
+  );
 }
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.selectedGenreId, nextProps.selectedGenreId);
+});
